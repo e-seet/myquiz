b0272f504208694650238ba3077323ad9eed6935
2202210 <2202210@sit.singaporetech.edu.sg>
2025-07-25 16:05:03 +0800

    change the test unit script

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -9,7 +9,7 @@ import os
 # Add the app directory to the Python path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-from app import app, validate_search_input
+from app.app import app, validate_search_input
 
 
 class TestSearchValidation:
