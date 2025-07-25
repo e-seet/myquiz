159a851a62a7238c06cb319f52dabe112fd081e0
2202210 <2202210@sit.singaporetech.edu.sg>
2025-07-25 17:05:05 +0800

    aadd wtf

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -75,6 +75,7 @@ class TestFlaskApp:
     def client(self):
         """Create a test client"""
         app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False 
         app.config['SECRET_KEY'] = 'test-secret-key'
         with app.test_client() as client:
             yield client
